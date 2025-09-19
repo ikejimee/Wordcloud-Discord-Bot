1547a13478c3f89abbf6d2cbc83c29a58b11fe95
Bethany <bethany.aung@gmail.com>
2025-09-19 18:15:25 -0500

    Added regex detectors

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hi")
+import re
+import io
+from wordcloud import WordCloud, STOPWORDS
+from PIL import image 
+
+# REGEX detectors
+URL_REGEX = re.compile(r"https?://\S+")
+MENTION_REGEX = re.compile(r"<@!?\d+>")
+CHANNEL_REGEX = re.compile(r"<#\d+>")
+ROLE_REGEX = re.compile(r"<@&\d+>")
+EMOJI_REGEX = re.compile(r"<a?:\w+\d+>")
+CODEBLOCK_REGEX = re.compile(r"```.*?```", re.DOTALL) # re.DOTALL means the . matches everything including newline characters
+INLINE_CODE_REGEX = re.compile(r"`[^`]+`")
+
